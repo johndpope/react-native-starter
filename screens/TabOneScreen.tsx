ce8d44e2cbd67bf6c1ea2d47a33d04da3a8ba6c9
yukiohori <yukiohori@gmail.com>
2021-10-17 00:52:16 +0900

    add: starter app added

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import tailwind from "tailwind-rn";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  return (
+    <View style={styles.container}>
+      <View style={tailwind("bg-blue-200 px-3 py-1 rounded-full")}>
+        <Text style={tailwind("text-blue-800 font-semibold")}>Tab One</Text>
+      </View>
+
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
